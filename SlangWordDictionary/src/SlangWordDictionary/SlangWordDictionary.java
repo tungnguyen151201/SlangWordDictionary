0f38e265d1739b07444f74e755d3911c5572107b
Huy Tùng Nguyễn <tungnguyen151201@hotmail.com>
2022-03-18 14:34:30 +0700

    Test commit 2

--- SlangWordDictionary/src/SlangWordDictionary/SlangWordDictionary.java
+++ SlangWordDictionary/src/SlangWordDictionary/SlangWordDictionary.java
@@ -3,6 +3,6 @@ package SlangWordDictionary;
 public class SlangWordDictionary {
 	public static void main(String[] args) {
 		System.out.println("Hello world!");
-		System.out.println("Test2");
+		System.out.println("Test3");
 	}
 }
