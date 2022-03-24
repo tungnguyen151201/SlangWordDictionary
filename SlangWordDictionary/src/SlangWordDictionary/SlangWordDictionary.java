b8a6db9a59b75aa45571d258581c7d51ba7be319
Huy Tùng Nguyễn <tungnguyen151201@hotmail.com>
2022-03-24 16:36:50 +0700

    Chức năng add slang word

--- SlangWordDictionary/src/SlangWordDictionary/SlangWordDictionary.java
+++ SlangWordDictionary/src/SlangWordDictionary/SlangWordDictionary.java
@@ -7,16 +7,23 @@ public class SlangWordDictionary {
 	public static List<String> history = new ArrayList<String>();
 	public static void ReadTextFile() {
 		try {
-			Scanner sc = new Scanner(new FileInputStream("slang.txt"));
-			ObjectOutputStream oos = new ObjectOutputStream(new FileOutputStream("slang.dat"));
+			Scanner sc = new Scanner(new FileInputStream("slang.txt"));			
 			HashMap<String, String> hm = new HashMap<String, String>();
 			while (sc.hasNextLine()) {
 				String line = sc.nextLine();
 				String[] tokens = line.split("`");
 				hm.put(tokens[0], tokens[1]);
-			}	
-			oos.writeObject(hm);
+			}
+			WriteDatFile(hm);
 			sc.close();
+		} catch (IOException e) {			
+			e.printStackTrace();
+		}
+	}
+	public static void WriteDatFile(HashMap<String, String> hm) {
+		try {
+			ObjectOutputStream oos = new ObjectOutputStream(new FileOutputStream("slang.dat"));
+			oos.writeObject(hm);
 			oos.close();
 		} catch (IOException e) {			
 			e.printStackTrace();
@@ -77,22 +84,33 @@ public class SlangWordDictionary {
 			System.out.println(sw);
 		}
 	}
+	public static void AddSlangWord(HashMap<String, String> hm) {
+		System.out.println("Nhap slang word muon add:");
+		Scanner sc3 = new Scanner(System.in);
+		String sw = sc3.nextLine();
+		System.out.println("Nhap definition:");
+		String def = sc3.nextLine();
+		hm.put(sw, def);
+		WriteDatFile(hm);
+		System.out.println("Add slang word thanh cong!");
+	}
 	public static void main(String[] args) {
 		File f = new File("slang.dat");
 		if (!f.exists()) {
 			ReadTextFile();
-		}
-		HashMap<String, String> hm = ReadDatFile();
-		if (hm == null) {
-			System.out.println("Loi doc dat file!");
-			return;
-		}
+		}		
 		while (true) {
+			HashMap<String, String> hm = ReadDatFile();
+			if (hm == null) {
+				System.out.println("Loi doc dat file!");
+				return;
+			}
 			System.out.println("Chon chuc nang thuc hien:");
 			System.out.println("0. Thoat chuong trinh");
 			System.out.println("1. Tim kiem theo slang word");
 			System.out.println("2. Tim kiem theo definition");
 			System.out.println("3. Xem lich su tim kiem slang word");
+			System.out.println("4. Add slang word");
 			Scanner sc = new Scanner(System.in);
 			int option = sc.nextInt();
 			switch (option) {
@@ -107,6 +125,9 @@ public class SlangWordDictionary {
 				break;
 			case 3:
 				HistorySlangWord();
+				break;
+			case 4:
+				AddSlangWord(hm);
 				break;
 			}
 		}
